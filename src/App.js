30d9c469eddc94f9898c2f1934ea3b347dd6f6be
Daron Schmit <darons23@gmail.com>
2021-02-27 20:05:03 -0600

    gutting original files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import employees from './employees.json';
+import EmployeeCard from './components/EmployeeCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {employees.map(employee => (
+          <EmployeeCard
+            name={employee.image}
+            title={employee.occupation}
+            startDate={employee.startDate}
+          />
+        ))}
       </header>
     </div>
   );
--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -7,3 +7,5 @@ function EmployeeCard(props){
         </div>
     )
 }
+
+export default EmployeeCard;
