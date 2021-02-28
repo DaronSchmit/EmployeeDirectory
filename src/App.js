898f8154c9da7bd38ad5bfcc646c8e00b2acd5aa
Daron Schmit <darons23@gmail.com>
2021-02-27 20:06:20 -0600

    got something working in app.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {employees.map(employee => (
           <EmployeeCard
-            name={employee.image}
-            title={employee.occupation}
-            startDate={employee.startDate}
+            name={employees[0].name}
+            title={employees[0].title}
+            startDate={employees[0].startDate}
           />
-        ))}
       </header>
     </div>
   );
