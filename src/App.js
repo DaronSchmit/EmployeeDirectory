4e88f01b1baf917abd66bea61c30f5893810b72b
Daron Schmit <darons23@gmail.com>
2021-02-28 01:07:22 -0600

    added sorting by name

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
-import './App.css';
-import employees from './employees.json';
-import EmployeeCard from './components/EmployeeCard';
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  // Setting this.state.employees to the employees json array
+  state = {
+    employees
+  };
+
+  removeEmployee = id => {
+    // Filter this.state.employees for employees with an id not equal to the id being removed
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    // Set this.state.employees equal to the new employees array
+    this.setState({ employees });
+  };
+
+  //pulled sorting from stackoverflow
+  compareName = ( a, b ) => {
+    if ( a.name < b.name ){
+      return -1;
+    }
+    if ( a.name > b.name ){
+      return 1;
+    }
+    return 0;
+  }
+  
+  sortByName = (objs) => {
+    console.log(objs)
+    this.setState(objs.sort(this.compareName));
+  };
+
+  // Map over this.state.employees and render a employeeCard component for each employee object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employee Directory</Title>
+        <button onClick={() => this.sortByName(employees)}>Sort by Name</button>
+        {this.state.employees.map(employee => (
           <EmployeeCard
-            name={employees[0].name}
-            title={employees[0].title}
-            startDate={employees[0].startDate}
+            removeEmployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            occupation={employee.occupation}
+            location={employee.location}
           />
-      </header>
-    </div>
-  );
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
