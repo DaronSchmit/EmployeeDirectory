59b230e2bf9501c1591532149559b1449d7ca650
DaronS23 <darons23@gmail.com>
2021-03-03 01:14:31 -0600

    cleaning up some UI stuff. Still looks bad

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,19 +3,22 @@ import Interact from "../Interact";
 
 function Form (props) {
     return (
-      <form className="form">
-          <select onChange={props.handleQueryChange}>
-            <option id="0">search</option>
-            <option id="2">sort</option>
-          </select>
-          By 
-          <select onChange={props.handleAttributeChange}>
-            <option id="0">name</option>
-            <option id="1">registered</option>
-            <option id="2">email</option>
-          </select>
-          <Interact data={props.data} handleOnClick={props.handleOnClick} handleInputChange={props.handleInputChange}/>
-        </form>
+      <div className="col-6 offset-4">
+          <form className="form" aira-label="Default select">
+              <select className="form-select" onChange={props.handleQueryChange}>
+                <option id="0">search</option>
+                <option id="2">sort</option>
+              </select>
+              By 
+              <select onChange={props.handleAttributeChange}>
+                <option id="0">name</option>
+                <option id="1">registered</option>
+                <option id="2">email</option>
+              </select>
+              <Interact data={props.data} handleOnClick={props.handleOnClick} handleInputChange={props.handleInputChange}/>
+            </form>
+      </div>
+
     );
   }
 
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -4,9 +4,7 @@ import Form from "../Form";
 function Search (props){
     return(
         <div className="row">
-            <div className="col">
                 <Form handleOnClick={props.handleOnClick} handleAttributeChange={props.handleAttributeChange} handleQueryChange={props.handleQueryChange} handleInputChange={props.handleInputChange} data={props.data} />
-            </div>
         </div>
     )
 }
